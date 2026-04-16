bedf7bf9eb11017a1adb96500619b601a43a2320
tusshr <d3v.tushar@gmail.com>
2026-04-17 00:04:00 +0600

    chore: configure eslint and prettier
    Add Prettier configuration and ignore rules, and enhance ESLint with additional plugins for file naming conventions, Node.js best practices, and Prettier compatibility. Includes installation of necessary development dependencies.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,15 @@
-import { defineConfig, globalIgnores } from "eslint/config";
+import tsParser from "@typescript-eslint/parser";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier/flat";
+import checkFile from "eslint-plugin-check-file";
+import n from "eslint-plugin-n";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettier,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
@@ -12,7 +17,49 @@ const eslintConfig = defineConfig([
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "**/*.config.{js,cjs,mjs,ts}",
+    // Generated test artifacts:
+    "playwright-report/**",
+    "test-results/**",
+    "coverage/**",
   ]),
+  {
+    plugins: {
+      "check-file": checkFile,
+      n,
+    },
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 5,
+      sourceType: "script",
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    rules: {
+      "prefer-arrow-callback": ["error"],
+      "prefer-template": ["error"],
+      semi: ["error"],
+      quotes: ["error", "double"],
+      "n/no-process-env": ["error"],
+      "no-unused-vars": ["error"],
+      "check-file/filename-naming-convention": [
+        "error",
+        {
+          "**/*.{ts,tsx}": "KEBAB_CASE|?(_)*",
+        },
+        {
+          ignoreMiddleExtensions: true,
+        },
+      ],
+      "check-file/folder-naming-convention": [
+        "error",
+        {
+          "src/**/!^[.*": "KEBAB_CASE",
+        },
+      ],
+    },
+  },
 ]);
 
 export default eslintConfig;
